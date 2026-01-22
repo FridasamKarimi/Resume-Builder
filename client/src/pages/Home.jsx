aa413655174887e1bbb1f24ff3b8121e3d559b46
FridasamKarimi <muriithifridasam@gmail.com>
2026-01-23 01:25:40 +0300

    new updates

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Banner from '../components/home/Banner'
+import Hero from '../components/home/Hero'
+import Features from '../components/home/Features'
 
 const Home = () => {
   return (
     <div>
-      <hi>Home Page</hi>
+      <Banner />
+      <Hero />
+      <Features />
     </div>
   )
 }
